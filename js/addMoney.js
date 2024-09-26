637dc357ba98f6288da8ae6af9650f48f17a56a3
Md. Rabiul Islam <rabiul77777888@gmail.com>
2024-09-26 23:45:24 +0600

    show transactions, dynamic DOM and data validation

--- js/addMoney.js
+++ js/addMoney.js
@@ -23,6 +23,13 @@ document.getElementById('btn-add-money').addEventListener('click', function(even
        const addMoney = getInputFieldValueById('input-add-money');
        const pinNumber = getInputFieldValueById('input-pin-number');
 
+
+       if(isNaN(addMoney))
+       {
+         alert('Failed to add money');
+         return;
+       }
+
        // wrong way to verify. do not try it at your serious website
       if(pinNumber === 1234){
            const balance = getTextFieldValueById('account-balance');
@@ -30,6 +37,15 @@ document.getElementById('btn-add-money').addEventListener('click', function(even
            const newBalance = balance + addMoney;
          
            document.getElementById('account-balance').innerText = newBalance;
+
+           // add to transaction history
+           const p = document.createElement('p');
+           p.innerText = `Added: ${addMoney} Tk. New Balance: ${newBalance}`;
+
+           console.log(p);
+
+           // should be a common function
+           document.getElementById('transaction-container').appendChild(p);
 
       }
       else{
